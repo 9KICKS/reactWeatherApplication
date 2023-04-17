4d2dd70836e90be525aae6fd0bf7f32a800edbcc
9KICKS <officialjoshua9@gmail.com>
2023-04-17 20:56:41 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Weather from "./pages/dashboard/components/Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
